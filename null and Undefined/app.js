6732363d5064f4fa02365592cbb3f847ce88b4fa
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-20 15:43:48 -0500

    null and undefined

---
+++ null and Undefined/app.js
@@ -0,0 +1,11 @@
+// null and undefines has no value
+
+// undefined javascript cant value for instanceof
+
+// null if it has only null it return null
+let value = 20 + null; //20  +  0
+console.log(value);
+
+// undefined if it has undefined it return undefined
+let value2 = 20 + undefined;
+console.log(value2);

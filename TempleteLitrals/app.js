27a0f897a140681107b8f6f2c76fdb6de0abf811
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-20 12:48:59 -0500

    template literals

---
+++ TempleteLitrals/app.js
@@ -0,0 +1,5 @@
+const name = "John";
+const age = 30;
+
+// Template Literals - ES6 feature to allow us to use string and variables in a quote the $ sign is used to represent the variable and `` is used to wrap the string and variables in quotes
+console.log(`My name is ${name} and I am ${age}  years old`);

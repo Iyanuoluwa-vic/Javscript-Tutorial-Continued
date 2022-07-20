7032b6e08557f532ad5e7aa9fde184ff3c894243
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-20 16:05:38 -0500

    ternary operator

---
+++ ternary operator/app.js
@@ -0,0 +1,9 @@
+// writing th if and else statment
+if (1 < 0) {
+  console.log("it is true");
+} else {
+  console.log("it is false");
+}
+
+// wrting if and else with the ternary operator
+1 < 0 ? console.log("it is true") : console.log("it is false");

c6853cbea3994d8efdfa5c272b9ae7f31b73bffd
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-07-26 19:03:01 -0500

    forEach

--- forEach/app.js
+++ forEach/app.js
@@ -1,10 +1,23 @@
-const people = [{ name: 'John', age: 20 }, { name: 'Peter', age: 30 }, { name: 'Mary', age: 25 }];
+const people = [
+  { name: "John", age: 20 },
+  { name: "Peter", age: 30 },
+  { name: "Mary", age: 25 },
+];
 
-
-
-function showPerson(person){
-   console.log(person);
-
-
-   
+function showPerson(person) {
+  console.log(person);
+  console.log(person.name.toUpperCase());
 }
+
+// The forEach() method calls a function for each element in an array.
+people.forEach(showPerson);
+
+const car = [
+  { name: "jaguar", year: 1956 },
+  { name: "toyota", year: 1922 },
+];
+
+// The forEach() method calls a function for each element in an array.
+car.forEach(function (cars) {
+  console.log(cars);
+});

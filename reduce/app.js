e8d8fe636bf168cf730b89e3c97e7e93126fe435
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-08-02 19:12:19 -0500

    reduce

--- reduce/app.js
+++ reduce/app.js
@@ -1,3 +1,7 @@
+// reduce to a single value
+// iterate callback fuction
+// 1st parameter ("acc")=total of all caulculation
+// 2st parameter ("curr")=current iteration/value
 const people = [
   { name: "victor", age: 12, position: "sof ", id: 1, salary: 200 },
   { name: "james", age: 122, position: "sd", id: 1, salary: 200 },
@@ -5,9 +9,9 @@ const people = [
 ];
 
 const total = people.reduce(function (acc, curr) {
-  console.log(`total ${acc}`);
-  console.log(`current money: ${curr.salary}`);
-  acc += curr;
+  // console.log(`total ${acc}`);
+  // console.log(`current money: ${curr.salary}`);
+  acc += curr.salary;
   return acc;
 }, 0);
 

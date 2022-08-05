63e08a2bdab0c06afcc19c5800519657c5ac0a84
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-08-05 09:55:23 -0500

    navigate the DOM with .nextElementSibling and .previousElementSibling

---
+++ Dom/navigate the Dom -nextElementSibling ,previousElementSibling/app.js
@@ -0,0 +1,9 @@
+// the .nextElementSibling return the next element after the specfic elemtn
+const first = document.querySelector(".first");
+const nextElement = first.nextElementSibling;
+console.log(nextElement);
+
+// the .previousElementSibling return the previous element
+const last = document.querySelector(".last");
+const previousElement = last.previousElementSibling;
+console.log(previousElement);

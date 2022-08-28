c6dc681b5b2a67a6964ea53ba339122dafe30616
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-08-28 16:58:33 -0500

    numbered

---
+++ Dom/2 getElementById/app.js
@@ -0,0 +1,9 @@
+//it is used to get element id from the html
+// you could then had some property to it
+
+const h1 = document.getElementById("element");
+h1.style.color = "red";
+
+document.getElementById("btn").style.backgroundColor = "yellow";
+const heading = document.getElementsByTagName("h2");
+console.log(heading.length);

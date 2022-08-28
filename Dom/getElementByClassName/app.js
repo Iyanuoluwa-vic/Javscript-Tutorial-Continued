6423ac5809ab2ce8372f48fbaaa3581a149d1e5f
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-08-28 17:01:11 -0500

    numbered

--- Dom/getElementByClassName/app.js
+++
@@ -1,3 +0,0 @@
-const headings = document.getElementsByClassName("h1");
-headings[0].style.backgroundColor = "indigo";
-console.log(headings);
--- Dom/navigate the Dom -nextSibling ,previousSibling/app.js
+++
@@ -1,9 +0,0 @@
-// the .nextSibling.nextSibling return the next element after the specfic elemtn
-const first = document.querySelector(".first");
-const nextElement = first.nextSibling.nextSibling;
-console.log(nextElement);
-
-// the .previousSibling.previousSibling return the previous element
-const last = document.querySelector(".last");
-const previousElement = last.previousSibling.previousSibling;
-console.log(previousElement);
